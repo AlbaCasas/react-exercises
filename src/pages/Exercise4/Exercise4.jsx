b9a302dafabb2789b54f2aeba81e99e06a9e099b
AlbaCasas <albacasasarzua1b@gmail.com>
2021-11-27 21:16:58 +0100

    DAdd Onclick to buttons using useState

--- src/pages/Exercise4/Exercise4.jsx
+++ src/pages/Exercise4/Exercise4.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
 const Exercise4 = () => {
+  const [text, setText] = useState();
   return (
     <div>
-      <button>Yes</button>
-      <button>No</button>
+      <button onClick={() => setText("Yes was clicked")}>Yes</button>
+      <button onClick={() => setText("No was clicked")}>No</button>
+      <p>{text}</p>
     </div>
   );
 };
