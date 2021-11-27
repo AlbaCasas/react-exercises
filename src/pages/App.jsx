e6843ff2d1c12a67b3b2de1abb3d0f6d9b2960c8
AlbaCasas <albacasasarzua1b@gmail.com>
2021-11-27 20:31:29 +0100

    Create EExercise2 route and add the button

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Exercise1 from "./Exercise1";
+import Exercise2 from "./Exercise2";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="exercise-1" element={<Exercise1 />} />
+        <Route path="exercise-2" element={<Exercise2 />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Exercise2/Exercise2.jsx
@@ -0,0 +1,10 @@
+const Exercise2 = () => {
+  return (
+    <div>
+      <button>Click me</button>
+      <p>I have been clicked 0 times</p>
+    </div>
+  );
+};
+
+export default Exercise2;
