a3beb9b2f108d2034d49de12e97d467d3fa0628f
AlbaCasas <albacasasarzua1b@gmail.com>
2021-11-27 21:00:20 +0100

    Create Yes and No buttons

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Exercise1 from "./Exercise1";
-import Exercise3 from "./Exercise3";
 import Exercise2 from "./Exercise2";
+import Exercise3 from "./Exercise3";
+import Exercise4 from "./Exercise4";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="exercise-1" element={<Exercise1 />} />
-        <Route path="exercise-3" element={<Exercise3 />} />
         <Route path="exercise-2" element={<Exercise2 />} />
+        <Route path="exercise-3" element={<Exercise3 />} />
+        <Route path="exercise-4" element={<Exercise4 />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Exercise4/Exercise4.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const Exercise4 = () => {
+  return (
+    <div>
+      <button>Yes</button>
+      <button>No</button>
+    </div>
+  );
+};
+
+export default Exercise4;
