5a124fce8f90d2ebc39a3d7665d795d77554791b
AlbaCasas <albacasasarzua1b@gmail.com>
2021-11-28 21:09:53 +0100

    Remove unneded abstraction

--- src/pages/Exercise5/Exercise5.jsx
+++ src/pages/Exercise5/Exercise5.jsx
@@ -3,13 +3,15 @@ import { useState } from "react";
 const Exercise5 = () => {
   const [posts, setPosts] = useState([]);
 
-  const handleButtonClick = () => {
-    setPosts([...posts, "Hello I'm a post"]);
-  };
-
   return (
     <div>
-      <button onClick={handleButtonClick}>New post</button>
+      <button
+        onClick={() => {
+          setPosts([...posts, "Hello I'm a post"]);
+        }}
+      >
+        New post
+      </button>
       {posts.map((post) => {
         return <p>{post}</p>;
       })}
