ca69266281820050042a891cd85e9e64bdfda4c8
AlbaCasas <albacasasarzua1b@gmail.com>
2021-11-28 21:06:26 +0100

    Add post when clicking in button

--- src/pages/Exercise5/Exercise5.jsx
+++ src/pages/Exercise5/Exercise5.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 const Exercise5 = () => {
-  return <button>New post</button>;
+  const [posts, setPosts] = useState([]);
+
+  const handleButtonClick = () => {
+    setPosts([...posts, "Hello I'm a post"]);
+  };
+
+  return (
+    <div>
+      <button onClick={handleButtonClick}>New post</button>
+      {posts.map((post) => {
+        return <p>{post}</p>;
+      })}
+    </div>
+  );
 };
 
 export default Exercise5;
