886cf288492eb004fd6b5649248b925a861095fa
AlbaCasas <albacasasarzua1b@gmail.com>
2021-11-30 08:20:34 +0100

    Create input and button in smart-blog

---
+++ src/pages/Exercise6/Exercise6.jsx
@@ -0,0 +1,10 @@
+const Excercise6 = () => {
+  return (
+    <div>
+      <input placeholder="Write the content" />
+      <button>New post</button>
+    </div>
+  );
+};
+
+export default Excercise6;
