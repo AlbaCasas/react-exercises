cd21c73f82562f907bd5528e68ce5ed26077195d
AlbaCasas <albacasasarzua1b@gmail.com>
2021-11-30 09:27:47 +0100

    DAdd tasks when you click in button

--- src/pages/Exercise6/Exercise6.jsx
+++ src/pages/Exercise6/Exercise6.jsx
@@ -1,9 +1,23 @@
-const Excercise6 = () => {
+import { useState } from "react";
+
+const Excercise6 = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  const handlePostSubmit = (event) => {
+    event.preventDefault();
+    const inputValue = event.target[0].value;
+    if (inputValue !== "") {
+      setPosts([...posts, inputValue]);
+    }
+  };
   return (
-    <div>
+    <form onSubmit={handlePostSubmit}>
       <input placeholder="Write the content" />
       <button>New post</button>
-    </div>
+      {posts.map((post) => {
+        return <p>{post}</p>;
+      })}
+    </form>
   );
 };
 
