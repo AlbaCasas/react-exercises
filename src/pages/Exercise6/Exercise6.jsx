17f991019118832d178f3f343e36a0f31e705af0
AlbaCasas <albacasasarzua1b@gmail.com>
2021-11-30 16:29:00 +0100

    Refactor INPUT_NAME and move posts outside form

--- src/pages/Exercise6/Exercise6.jsx
+++ src/pages/Exercise6/Exercise6.jsx
@@ -1,23 +1,26 @@
 import { useState } from "react";
 
+const INPUT_NAME = "postInput";
 const Excercise6 = (props) => {
   const [posts, setPosts] = useState([]);
 
   const handlePostSubmit = (event) => {
     event.preventDefault();
-    const inputValue = event.target[0].value;
+    const inputValue = event.target[INPUT_NAME].value;
     if (inputValue !== "") {
       setPosts([...posts, inputValue]);
     }
   };
   return (
-    <form onSubmit={handlePostSubmit}>
-      <input placeholder="Write the content" />
-      <button>New post</button>
+    <div>
+      <form onSubmit={handlePostSubmit}>
+        <input placeholder="Write the content" name={INPUT_NAME} />
+        <button>New post</button>
+      </form>
       {posts.map((post) => {
         return <p>{post}</p>;
       })}
-    </form>
+    </div>
   );
 };
 
